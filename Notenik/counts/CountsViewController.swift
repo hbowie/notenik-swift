1960c522090907b2a915dd8c562b7331dc78ed8c
Herb Bowie <hbowie@mac.com>
2022-08-21 09:44:24 -0700

    Added Minutes to Read to Counts Window

--- Notenik/counts/CountsViewController.swift
+++ Notenik/counts/CountsViewController.swift
@@ -3,7 +3,7 @@
 //  Notenik
 //
 //  Created by Herb Bowie on 5/28/20.
-//  Copyright © 2020 Herb Bowie (https://powersurgepub.com)
+//  Copyright © 2020 - 2022 Herb Bowie (https://powersurgepub.com)
 //
 //  This programming code is published as open source software under the
 //  terms of the MIT License (https://opensource.org/licenses/MIT).
@@ -19,23 +19,24 @@ class CountsViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
     @IBOutlet var tableView: NSTableView!
     
     var counts = MkdownCounts()
+    var minutesToRead: MinutesToReadValue!
     
     let fixedPitchFont = NSFont.userFixedPitchFont(ofSize: 13.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
     }
     
     func updateCounts(_ counts: MkdownCounts) {
         self.counts = counts
+        minutesToRead = MinutesToReadValue(with: self.counts)
         if self.isViewLoaded {
             tableView.reloadData()
         }
     }
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 5
+        return 6
     }
     
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
@@ -50,6 +51,7 @@ class CountsViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
             case 2: str = "Number of Words............. "
             case 3: str = "Number of Text Characters... "
             case 4: str = "Average Word Length......... "
+            case 5: str = "Minutes to Read............. "
             default: str = ""
             }
         } else {
@@ -59,6 +61,7 @@ class CountsViewController: NSViewController, NSTableViewDelegate, NSTableViewDa
             case 2: str = "\(counts.words)"
             case 3: str = "\(counts.text)"
             case 4: str = "\(Double(round(100*counts.averageWordLength)/100))"
+            case 5: str = "\(minutesToRead.value)"
             default: str = ""
             }
         }
