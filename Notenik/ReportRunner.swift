24b4d9211c4e10aa7b91dcd287f8291877762c9d
Herb Bowie <hbowie@mac.com>
2023-12-27 18:46:00 -0800

    Expanded use of ReportRunner

---
+++ Notenik/ReportRunner.swift
@@ -0,0 +1,13 @@
+//
+//  ReportRunner.swift
+//  Notenik
+//
+//  Created by Herb Bowie on 12/27/23.
+//  Copyright © 2023 PowerSurge Publishing. All rights reserved.
+//
+
+import Cocoa
+
+class ReportRunner: NSObject {
+
+}
